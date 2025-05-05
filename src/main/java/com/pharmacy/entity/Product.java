b6776caf6440b94f2286729d34494a1814597705
imaneboho2159 <imaboh21@gmail.com>
2025-05-05 14:42:46 +0100

    generate the product entity

--- src/main/java/com/pharmacy/entity/Product.java
+++ src/main/java/com/pharmacy/entity/Product.java
@@ -1,11 +1,82 @@
 package com.pharmacy.entity;
 
 
+import jakarta.persistence.*;
 
-
-
+@Entity
+@Table(name="Product")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name= "product_name")
+    private String name;
+    @Column
+    private double price;
+    @Column
+    private int quantity;
+    @Column
+    private String description;
 
+    public Product() {
+    }
 
+    public Product(String name, double price, int quantity, String description) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
