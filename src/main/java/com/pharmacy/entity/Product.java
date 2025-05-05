fac9f06d99c14054add2650b6e74bde6849b6396
imaneboho2159 <imaboh21@gmail.com>
2025-05-05 13:00:26 +0100

    Initial commit

---
+++ src/main/java/com/pharmacy/entity/Product.java
@@ -0,0 +1,11 @@
+package com.pharmacy.entity;
+
+
+
+
+
+public class Product {
+
+
+
+}
---
+++ src/test/java/com/pharmacyapp/PharmacyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmacyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
