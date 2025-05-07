150cd0c335693d8153118d4f29066e4da61f98fc
imaneboho2159 <imaboh21@gmail.com>
2025-05-07 15:45:49 +0100

    create the function of getProductById

--- src/main/java/com/pharmacyapp/controller/ProductController.java
+++ src/main/java/com/pharmacyapp/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.pharmacyapp.model.Product;
 import com.pharmacyapp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,10 @@ public class ProductController {
     @GetMapping
     public List<Product> getAllProduct(){
         return productService.getAllProducts();
+    }
+    @GetMapping("/{id}")
+    public  Product getProductById(@PathVariable Long id){
+        return productService.getProductById(id).orElseThrow();
     }
 
 }
--- src/main/java/com/pharmacyapp/service/ProductService.java
+++ src/main/java/com/pharmacyapp/service/ProductService.java
@@ -5,8 +5,11 @@ import com.pharmacyapp.model.Product;
 import com.pharmacyapp.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -20,4 +23,8 @@ public class ProductService {
     public List<Product> getAllProducts(){
     return productRepository.findAll();
     }
+public Optional<Product> getProductById(Long id){
+    return productRepository.findById(id);
+}
+
 }
